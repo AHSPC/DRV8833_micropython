44cf60b4aa4034f71d5d939384d56ed84e4e6b93
Jack N <nystromjp@gmail.com>
2024-03-27 15:04:57 -0700

    overhaul

--- drv8833.py
+++ drv8833.py
@@ -2,11 +2,26 @@ from machine import PWM
 
 
 class DRV8833:
-    def throttle_a(self, throttle: float):
-        self.__throttle(self.motor_a_in_1, self.motor_a_in_2, throttle)
+    """
+    Basic helper class to manipulate the DRV8833 dual motor controller.
+    Partially based on adafruit's adafruit_motor library, but with custom
+    decisions made for our usecases. NOTE: this helper does not currently
+    support both stopping methods (high-z and low-z), but only uses low-z.
+    Note about throttle()'s decay_mode parameter: SLOW_DECAY is recommended
+    to improve spin threshold, speed-to-throttle linearity, and PWM
+    frequency sensitivity. We use this as the default.
+    """
 
-    def throttle_b(self, throttle: float):
-        self.__throttle(self.motor_b_in_1, self.motor_b_in_2, throttle)
+    FAST_DECAY = 0
+    SLOW_DECAY = 1
+    MAX_DUTY_CYCLE = 0xFFFF  # 65535
+    MIN_DUTY_CYCLE = 0
+
+    def throttle_a(self, throttle: float, decay_mode: int = SLOW_DECAY):
+        self.__throttle(self.motor_a_in_1, self.motor_a_in_2, throttle, decay_mode)
+
+    def throttle_b(self, throttle: float, decay_mode: int = SLOW_DECAY):
+        self.__throttle(self.motor_b_in_1, self.motor_b_in_2, throttle, decay_mode)
 
     def stop_a(self):
         self.__stop(self.motor_a_in_1, self.motor_a_in_2)
@@ -24,41 +39,40 @@ class DRV8833:
         pass
 
     def __stop(self, pin1: PWM, pin2: PWM):
-        pin1.duty_u16(self.min_duty_cycle)
-        pin2.duty_u16(self.min_duty_cycle)
-        print("__stop")
+        pin1.duty_u16(self.MIN_DUTY_CYCLE)
+        pin2.duty_u16(self.MIN_DUTY_CYCLE)
 
-    def __throttle(self, pin1: PWM, pin2: PWM, throttle: float):
-        duty_cycle = int(
-            abs(throttle) * (self.max_duty_cycle - self.min_duty_cycle)
-            + self.min_duty_cycle
-        )
-        if throttle == 0:
-            self.__stop(pin1, pin2)
-        elif 0 < throttle <= 1:
-            pin1.duty_u16(duty_cycle)
-            pin2.duty_u16(self.min_duty_cycle)
-        elif -1 <= throttle < 0:
-            pin1.duty_u16(self.min_duty_cycle)
-            pin2.duty_u16(duty_cycle)
-        else:
+    def __throttle(self, pin1: PWM, pin2: PWM, throttle: float, decay_mode: int):
+        if not -1.0 <= throttle <= 1.0:
             raise ValueError("Throttle value is out of range [ -1, 1 ]!")
 
-    def __init__(
-        self,
-        ain1,
-        ain2: PWM,
-        bin1: PWM,
-        bin2: PWM,
-        max_duty_cycle=65535,
-        min_duty_cycle=0,
-    ) -> None:
+        duty_cycle = int(abs(throttle) * self.MAX_DUTY_CYCLE)
+        if throttle == 0:
+            self.__stop(pin1, pin2)
+        if decay_mode == self.SLOW_DECAY:
+            if 0 < throttle <= 1:
+                pin1.duty_u16(self.MAX_DUTY_CYCLE - duty_cycle)
+                pin2.duty_u16(self.MAX_DUTY_CYCLE)
+            elif -1 <= throttle < 0:
+                pin1.duty_u16(self.MAX_DUTY_CYCLE)
+                pin2.duty_u16(self.MAX_DUTY_CYCLE - duty_cycle)
+        elif decay_mode == self.FAST_DECAY:
+            if 0 < throttle <= 1:
+                pin1.duty_u16(duty_cycle)
+                pin2.duty_u16(self.MIN_DUTY_CYCLE)
+            elif -1 <= throttle < 0:
+                pin1.duty_u16(self.MIN_DUTY_CYCLE)
+                pin2.duty_u16(duty_cycle)
+        else:
+            raise ValueError(
+                "decay_mode must be one of either FAST_DECAY or SLOW_DECAY!"
+            )
+
+    def __init__(self, ain1, ain2: PWM, bin1: PWM, bin2: PWM) -> None:
         self.motor_a_in_1 = ain1
         self.motor_a_in_2 = ain2
         self.motor_b_in_1 = bin1
         self.motor_b_in_2 = bin2
-        self.max_duty_cycle = max_duty_cycle
-        self.min_duty_cycle = min_duty_cycle
 
     def __del__(self):
         self.deinit()
